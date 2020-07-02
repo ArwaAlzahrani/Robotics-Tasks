71244df1e9ded88198be8103a395da3f764a1987
Arwa Alzahrani <67156314+ArwaAlzahrani@users.noreply.github.com>
2020-07-02 17:44:54 +0300

    Task4
    Control the direction of a DC motor using relay master.
    Collaborative work of trainees.

---
+++ DC_moto_Relay.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
